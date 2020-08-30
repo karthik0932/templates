c51154b47de10406a7698e7ad312700e4250f073
mrjeevan <mrjeevan2000@gmail.com>
2020-08-30 10:48:07 +0530

    adding features

--- Hacker-theme.js
+++ Hacker-theme.js
@@ -1,7 +1,10 @@
 var x = document.getElementsByTagName("h1");
+const title = document.getElementsByTagName("title");
 for(var i=0; i<x.length;i++ ){
     x[i].innerHTML = './ '+x[i].innerHTML;
+    // x[i].innerHTML = title[0].innerText + ':$ '+x[i].innerHTML;
 }
+
 
 
 
